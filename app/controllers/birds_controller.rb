f74eceb2e8f5496f5e467ae8cde353123049eeaf
keowen375@gmail.com <keowen375@gmail.com>
2023-07-03 08:45:04 +0300

    Complete Task

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,4 @@
 class BirdsController < ApplicationController
-
   # GET /birds
   def index
     birds = Bird.all
@@ -14,6 +13,11 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
   end
 
 end
